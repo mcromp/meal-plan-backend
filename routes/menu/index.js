3a51a73598f56f0c978d1a6ae064e1bf3fb875b9
Matthew Crompton <crompton.ml@gmail.com>
2020-09-14 18:55:27 +0200

    Cleanup menu route

--- routes/menu/index.js
+++ routes/menu/index.js
@@ -1,17 +1,23 @@
 const router = require("express").Router();
 let menu = require("./menu.json");
 
-router.route("/").get((req, res) => {
-  res.json(menu);
+router.get("/", async (req, res) => {
+  try {
+    res.json(menu);
+  } catch (err) {
+    res.status(500).json("Error: " + err);
+  }
 });
 
-router.route("/:id").get((req, res) => {
-  const id = req.params.id;
-  const menuItem = menu.find((menuItem) => menuItem.ID === id);
-  if (menuItem) {
-    res.json(menuItem);
-  } else {
-    res.status(404).end();
+router.get("/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    const menuItem = menu.find((menuItem) => menuItem.ID === id);
+    if (menuItem) {
+      res.json(menuItem);
+    }
+  } catch (err) {
+    res.status(400).json("Error: " + err);
   }
 });
 
