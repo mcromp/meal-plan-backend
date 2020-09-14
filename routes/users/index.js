317a1bb182d6a7f19129ffa5e39a5367ab3829f9
Matthew Crompton <crompton.ml@gmail.com>
2020-09-14 18:31:24 +0200

    Added favAdd patch

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,31 +1,75 @@
 const router = require("express").Router();
 const UserModel = require("./user.model");
+const menu = require("../menu/menu.json");
+const ID_LIST = menu.reduce((acc, item) => {
+  acc.push(item.ID);
+  return acc;
+}, []);
 
-router.route("/").get((req, res) => {
-  UserModel.find()
-    .then((users) => res.json(users))
-    .catch((err) => res.status(400).json("Error: " + err));
+//middleware
+const getUser = async (req, res, next) => {
+  let user;
+  try {
+    user = await UserModel.findById(req.params.id);
+    if (user == null) {
+      return res.status(404).json("Error: " + err);
+    }
+  } catch (err) {
+    return res.status(500).json("Error: " + err);
+  }
+  res.user = user;
+  next();
+};
+
+//get all
+router.route("/").get(async (req, res) => {
+  try {
+    const users = await UserModel.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json("Error: " + err);
+  }
 });
-
-//add new user
-router.route("/add").post((req, res) => {
-  console.log(req);
-  const username = req.body.username;
-  const favList = [];
-  const newUser = new UserModel({
-    username,
-    favList,
+//add user
+router.route("/signup").post(async (req, res) => {
+  const user = new UserModel({
+    username: req.body.username,
+    favList: [],
   });
-  newUser
-    .save()
-    .then(() => res.json("User added"))
-    .catch((err) => res.status(400).json(err));
+  try {
+    const newUser = await user.save();
+    res.status(201).json({ newUser });
+  } catch (err) {
+    res.status(400).json("Error: " + err);
+  }
 });
+
 //delete User
-router.route("/:id").delete((req, res) => {
-  UserModel.findByIdAndDelete(req.params.id)
-    .then(() => res.json("User deleted"))
-    .catch((err) => res.status(400).json(err));
+router.route("/:id").delete(async (req, res) => {
+  try {
+    await UserModel.findByIdAndDelete(req.params.id);
+    res.json("User deleted");
+  } catch (err) {}
+  res.status(500).json("Error: " + err);
 });
+
+//add fav
+router.patch("/:id/favAdd", getUser, async (req, res) => {
+  const { data } = req.body;
+  try {
+    if (!ID_LIST.includes(data)) {
+      throw new Error("Invalid ID");
+    }
+    if (res.user.favList.includes(data)) {
+      throw new Error("Already added");
+    }
+    res.user.favList.push(data);
+    const userWithUpdatedFavList = await res.user.save();
+    res.json(userWithUpdatedFavList);
+  } catch (err) {
+    res.status(400).json("Error: " + err);
+  }
+});
+//delete fav
 
 module.exports = router;
