6287099e28c7f19fc9233c7a83d5b29ecdcf41c9
Matthew Crompton <crompton.ml@gmail.com>
2020-09-23 16:00:08 +0200

    Remove id for calendar

--- routes/calendar/calendar.js
+++ routes/calendar/calendar.js
@@ -1,75 +1,72 @@
 const router = require("express").Router();
 const Calendar = require("./calendar.model");
-const { v4: uuidv4 } = require("uuid");
 
 //get all
 router.get("/", async (req, res) => {
-  try {
-    const calendar = await Calendar.find();
-    res.json(calendar);
-  } catch (err) {
-    res.status(500).json("Error: " + err);
-  }
+ try {
+  const calendar = await Calendar.find();
+  res.json(calendar);
+ } catch (err) {
+  res.status(500).json("Error: " + err);
+ }
 });
 //remove all calendar items for user
 router.delete("/:user/all", async (req, res) => {
-  try {
-    const deleted = await Calendar.deleteMany({
-      userId: req.params.user,
-    });
-    res.json(deleted);
-  } catch (err) {
-    res.status(500).json("Error: " + err);
-  }
+ try {
+  const deleted = await Calendar.deleteMany({
+   userId: req.params.user,
+  });
+  res.json(deleted);
+ } catch (err) {
+  res.status(500).json("Error: " + err);
+ }
 });
 
 //remove calendar items for a peticular day
 router.delete("/:user/:date", async (req, res) => {
-  try {
-    const deleted = await Calendar.deleteMany({
-      userId: req.params.user,
-      dateAdded: req.params.date,
-    });
-    res.json(deleted);
-  } catch (err) {
-    res.status(500).json("Error: " + err);
-  }
+ try {
+  const deleted = await Calendar.deleteMany({
+   userId: req.params.user,
+   dateAdded: req.params.date,
+  });
+  res.json(deleted);
+ } catch (err) {
+  res.status(500).json("Error: " + err);
+ }
 });
 
 //testing some stuff
 const removeCalendarDates = async (req, res, next) => {
-  const date = req.body[0].dateAdded;
-  try {
-    await Calendar.deleteMany({ dateAdded: `${date}` });
-  } catch (err) {
-    return res.status(500).json("Error: " + err);
-  }
-  next();
+ const date = req.body[0].dateAdded;
+ try {
+  await Calendar.deleteMany({ dateAdded: `${date}` });
+ } catch (err) {
+  return res.status(500).json("Error: " + err);
+ }
+ next();
 };
 
 //clear old calendar of day, add to calendar, several items
 router.post("/add", removeCalendarDates, async (req, res) => {
-  try {
-    const dataList = [...req.body];
-    const calendarList = dataList.reduce((acc, item) => {
-      const { foodItemId, dateAdded, quantity, userId } = item;
-      const id = uuidv4();
-      const calendar = new Calendar({
-        foodItemId,
-        dateAdded,
-        quantity,
-        userId,
-        id,
-      });
-      acc.push(calendar);
-      return acc;
-    }, []);
+ try {
+  const dataList = [...req.body];
+  const calendarList = dataList.reduce((acc, item) => {
+   const { foodItemId, dateAdded, quantity, userId } = item;
+   const calendar = new Calendar({
+    foodItemId,
+    dateAdded,
+    quantity,
+    userId,
+   });
+   acc.push(calendar);
+   return acc;
+  }, []);
 
-    await Calendar.insertMany(calendarList);
-    res.status(201).json(calendarList);
-  } catch (err) {
-    res.status(400).json("Error: " + err);
-  }
+  await Calendar.insertMany(calendarList);
+  res.status(201).json(calendarList);
+ } catch (err) {
+  res.status(400).json("Error: " + err);
+ }
 });
 
 module.exports = router;
--- routes/calendar/calendar.model.js
+++ routes/calendar/calendar.model.js
@@ -2,26 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const calendarSchema = new Schema({
-  foodItemId: {
-    type: String,
-    required: true,
-  },
-  dateAdded: {
-    type: String,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-  userId: {
-    type: String,
-    required: true,
-  },
-  id: {
-    type: String,
-    required: true,
-  },
+ foodItemId: {
+  type: String,
+  required: true,
+ },
+ dateAdded: {
+  type: String,
+  required: true,
+ },
+ quantity: {
+  type: Number,
+  required: true,
+ },
+ userId: {
+  type: String,
+  required: true,
+ },
 });
 
 const Calendar = mongoose.model("Calendar", calendarSchema);
