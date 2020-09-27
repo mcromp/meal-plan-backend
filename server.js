76664bba2390623c0730652b603607ad07066733
Matthew Crompton <crompton.ml@gmail.com>
2020-09-27 11:41:03 +0200

    Clean up server.js

--- server.js
+++ server.js
@@ -22,34 +22,12 @@ connection.once("open", () => {
  console.log("MongoDB database connection established successfully");
 });
 
-// const usersRouter = require("./routes/users");
-// app.use("/users", usersRouter);
-
 const menuRouter = require("./src/routes/menu");
 app.use("/menu", menuRouter);
 const userRouter = require("./src/routes/users");
 app.use("/users", userRouter);
 const calendarRouter = require("./src/routes/calendar");
 app.use("/calendar", calendarRouter);
-
-// app.get("/api/users/:name", (request, response) => {
-//   const name = request.params.name;
-//   const user = foodData.find((user) => {
-//     return user.name === name;
-//   });
-//   if (user) {
-//     response.json(user);
-//   } else {
-//     response.status(404).end();
-//   }
-// });
-
-// app.delete("/api/users/:name", (request, response) => {
-//   const name = request.params.name;
-//   notes = notes.filter((name) => name.id !== name);
-
-//   response.status(204).end();
-// });
 
 app.listen(port, () => {
  console.log(`Server is running on port: ${port}`);
