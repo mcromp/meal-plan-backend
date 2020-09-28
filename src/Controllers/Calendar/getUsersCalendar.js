beb728a8f0f7f5b852182efe81993c68ecb50f0d
Matthew Crompton <crompton.ml@gmail.com>
2020-09-28 14:02:47 +0200

    Fix getusercalendar bug

--- src/Controllers/Calendar/getUsersCalendar.js
+++ src/Controllers/Calendar/getUsersCalendar.js
@@ -2,7 +2,8 @@ const Calendar = require("../../Models/calendar.model");
 
 module.exports = async (req, res) => {
  try {
-  const calendar = await Calendar.find({ userId: req.query.userId });
+  console.log(req.query);
+  const calendar = await Calendar.find({ userId: req.params.userId });
   res.status(201).json(calendar);
  } catch (e) {
   res.status(500).json(`${e}`);
