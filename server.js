149c0c878487bd599ee9c59176a58a181fb2e93c
Matthew Crompton <crompton.ml@gmail.com>
2020-11-08 17:15:00 +0100

    add static build

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'))
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
