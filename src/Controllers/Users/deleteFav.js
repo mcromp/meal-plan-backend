40e00e3ad9b3c1a056e489977050183de6b9fbaf
Matthew Crompton <crompton.ml@gmail.com>
2020-11-10 17:21:13 +0100

    Prettier

--- src/Controllers/Users/deleteFav.js
+++ src/Controllers/Users/deleteFav.js
@@ -1,13 +1,15 @@
 module.exports = async (req, res) => {
-  const itemId = req.body.itemId;
-  const favList = res.user.favList;
-  const index = favList.indexOf(itemId);
-  try {
-    if (index === -1) { return res.status(400).json({ error: 'Cannot remove, item not found' }); }
-    res.user.favList.splice(index, 1);
-    const updatedUser = await res.user.save();
-    res.status(201).json(updatedUser);
-  } catch (e) {
-    res.status(400).json(e.message);
+ const itemId = req.body.itemId;
+ const favList = res.user.favList;
+ const index = favList.indexOf(itemId);
+ try {
+  if (index === -1) {
+   return res.status(400).json({ error: "Cannot remove, item not found" });
   }
+  res.user.favList.splice(index, 1);
+  const updatedUser = await res.user.save();
+  res.status(200).json(updatedUser);
+ } catch (e) {
+  res.status(400).json(e.message);
+ }
 };
