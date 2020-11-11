48ac8cd744b7fa789738d00429a0813c230cf152
Matthew Crompton <crompton.ml@gmail.com>
2020-11-11 12:55:36 +0100

    Add filter for easier troubleshoot

--- src/Controllers/Calendar/updateCalendar.js
+++ src/Controllers/Calendar/updateCalendar.js
@@ -1,15 +1,18 @@
-const Calendar = require('../../Models/calendar.model');
+const Calendar = require("../../Models/calendar.model");
 module.exports = async (req, res) => {
-  try {
-    const { menuItems, userId, date } = req.body;
-    const calendarItem = await Calendar.findOneAndUpdate(
-      { date, userId },
-      { menuItems: menuItems },
-      { new: true, upsert: true }
-    );
-    if (!calendarItem) { return res.status(400).json({ error: 'Date not found' }); }
-    res.status(201).json(calendarItem);
-  } catch (e) {
-    res.status(400).json(e.message);
+ try {
+  const { menuItems, userId, date } = req.body;
+  const filter = { date: date, userId: userId };
+  const calendarItem = await Calendar.findOneAndUpdate(
+   filter,
+   { menuItems: menuItems },
+   { new: true, upsert: true }
+  );
+  if (!calendarItem) {
+   return res.status(400).json({ error: "Date not found" });
   }
+  res.status(200).json(calendarItem);
+ } catch (e) {
+  res.status(400).json(e.message);
+ }
 };
