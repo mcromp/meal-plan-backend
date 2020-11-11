e59e193a6eeb5a49094af14c3c8af399e352a912
Matthew Crompton <crompton.ml@gmail.com>
2020-11-11 12:55:01 +0100

    add error handle for no user

--- src/Controllers/Calendar/getManyCalendar.js
+++ src/Controllers/Calendar/getManyCalendar.js
@@ -1,13 +1,18 @@
-const Calendar = require('../../Models/calendar.model');
+const Calendar = require("../../Models/calendar.model");
+const User = require("../../Models/user.model");
 
 module.exports = async (req, res) => {
-  try {
-    const calendar = await Calendar.find({
-      date: { $in: req.body.dateList },
-      userId: req.params.userId,
-    });
-    res.status(201).json(calendar);
-  } catch (e) {
-    res.status(500).json(e.message);
+ try {
+  const users = await User.findById(req.params.userId);
+  const calendar = await Calendar.find({
+   date: { $in: req.body.dateList },
+   userId: req.params.userId,
+  });
+  if (!users) {
+   return res.status(400).json({ error: "User Id not found" });
   }
+  res.status(200).json(calendar);
+ } catch (e) {
+  res.status(400).json(e.message);
+ }
 };
